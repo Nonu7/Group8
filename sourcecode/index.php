7ab453b4e8e5bc0dd069ec507c6f5d04360af7fa
Dev0555 <vermadv0000@gmail.com>
2023-09-27 00:21:09 +0530

    bug fixed

--- sourcecode/index.php
+++ sourcecode/index.php
@@ -1,127 +1,133 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Login Form</title>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="style.css">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login Form</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="style.css"> <!-- Link to your external CSS file -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <style>
-
-    body{
-        background-image: url('images/library.jpg')
+        body {
+            background-image: url('/library_Project/images/library.jpg');
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Full viewport height */
+        }
         
+        .container {
+            max-width: 500px;
+            padding: 20px;
+            background-color: rgba(255, 255, 255, 0.9);
+            border-radius: 5px;
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+        }
+        
+        .login-heading {
+            text-align: center;
+            margin-bottom: 20px;
+            font-size: 24px;
+            font-weight: bold;
+            color: #007BFF; /* Change the color to your preference */
+            text-transform: uppercase;
+        }
+        
+        .login-form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+        }
+        
+        .form-group {
+            margin-bottom: 20px;
+        }
+        
+        .btn-submit {
+            font-weight: 600;
+            color: #fff;
+            background-color: #007BFF;
+            border: none;
+        }
+        
+        .btn-submit:hover {
+            background-color: #0056b3;
+        }
+        
+        .error-message {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $msg = "";
+    $ademailmsg = "";
+    $adpasdmsg = "";
+    $emailmsg = "";
+    $pasdmsg = "";
+
+    if (!empty($_REQUEST['ademailmsg'])) {
+        $ademailmsg = $_REQUEST['ademailmsg'];
     }
 
+    if (!empty($_REQUEST['adpasdmsg'])) {
+        $adpasdmsg = $_REQUEST['adpasdmsg'];
+    }
 
-    .login-form-3 .btnSubmit {
-    font-weight: 600;
-    color: #0062cc;
-    background-color: #fff;
-}
+    if (!empty($_REQUEST['emailmsg'])) {
+        $emailmsg = $_REQUEST['emailmsg'];
+    }
 
-.login-form-3 h3 {
-    text-align: center;
-    color: #fff;
-}
+    if (!empty($_REQUEST['pasdmsg'])) {
+        $pasdmsg = $_REQUEST['pasdmsg'];
+    }
 
-.login-form-1 h3 {
-    text-align: center;
-    color: #fff;
-}
-.login-form-3 {
-    padding: 5%;
-    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
-}
+    if (!empty($_REQUEST['msg'])) {
+        $msg = $_REQUEST['msg'];
+    }
+    ?>
 
-    </style>
-    <body >
-
-<?php
- $emailmsg="";
- $pasdmsg="";
- $msg="";
-
- $ademailmsg="";
- $adpasdmsg="";
-
-
- if(!empty($_REQUEST['ademailmsg'])){
-    $ademailmsg=$_REQUEST['ademailmsg'];
- }
-
- if(!empty($_REQUEST['adpasdmsg'])){
-    $adpasdmsg=$_REQUEST['adpasdmsg'];
- }
-
- if(!empty($_REQUEST['emailmsg'])){
-    $emailmsg=$_REQUEST['emailmsg'];
- }
-
- if(!empty($_REQUEST['pasdmsg'])){
-  $pasdmsg=$_REQUEST['pasdmsg'];
-}
-
-if(!empty($_REQUEST['msg'])){
-    $msg=$_REQUEST['msg'];
- }
-
- ?>
-
-
-
-<div class="container login-container">
-<div class="row"><h4><?php echo $msg?></h4></div>
-            <div class="row">
- 
-                <div class="col-md-6 login-form-3">
-                    <h3>Admin Login</h3>
-                    <form action="loginadmin_server_page.php" method="get">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $ademailmsg?></label>
-                        <div class="form-group">
-                            <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $adpasdmsg?></label>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Login" />
-                        </div>
-                        <!-- <div class="form-group">
-
-                            <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
-                        </div> -->
-                    </form>
-                </div>
-                <div class="col-md-6 login-form-1">
-                    <h3>Student Login</h3>
-                    <form action="login_server_page.php" method="get">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $emailmsg?></label>
-                        <div class="form-group">
-                            <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $pasdmsg?></label>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Login" />
-                        </div>
-                    </form>
-                </div>
+    <div class="container">
+        <div class="row">
+            <h4><?php echo $msg ?></h4>
+        </div>
+        <div class="row">
+            <div class="col-md-6 login-form">
+                <h3 class="login-heading">Admin Login</h3>
+                <form action="loginadmin_server_page.php" method="get">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
+                        <label class="error-message"><?php echo $ademailmsg ?></label>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
+                        <label class="error-message"><?php echo $adpasdmsg ?></label>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-submit" value="Login" />
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6 login-form">
+                <h3 class="login-heading">Student Login</h3>
+                <form action="login_server_page.php" method="get">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
+                        <label class="error-message"><?php echo $emailmsg ?></label>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
+                        <label class="error-message"><?php echo $pasdmsg ?></label>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-submit" value="Login" />
+                    </div>
+                </form>
             </div>
         </div>
-
-
-
-        
-
-
-
-        <script src="" async defer></script>
-    </body>
+    </div>
+</body>
 </html>
