d999bf1ec05a68bcfdf763460531ede32454b7ef
Dev0555 <vermadv0000@gmail.com>
2023-09-28 09:45:53 +0530

    updated index

--- sourcecode/index.php
+++ sourcecode/index.php
@@ -3,131 +3,67 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login Form</title>
+    <title>Login Portal</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="style.css"> <!-- Link to your external CSS file -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet"> <!-- Link to Google Fonts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <style>
         body {
             background-image: url('/library_Project/images/library.jpg');
+            background-size: cover;
             display: flex;
             justify-content: center;
             align-items: center;
             height: 100vh; /* Full viewport height */
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
         }
-        
+
         .container {
             max-width: 500px;
             padding: 20px;
             background-color: rgba(255, 255, 255, 0.9);
             border-radius: 5px;
             box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
-        }
-        
-        .login-heading {
             text-align: center;
+        }
+
+        h1 {
+            font-size: 28px;
+            color: #007BFF;
             margin-bottom: 20px;
-            font-size: 24px;
+        }
+
+        p {
+            font-size: 18px;
+            margin-bottom: 30px;
+        }
+
+        button {
+            font-size: 16px;
             font-weight: bold;
-            color: #007BFF; /* Change the color to your preference */
-            text-transform: uppercase;
-        }
-        
-        .login-form {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-        }
-        
-        .form-group {
-            margin-bottom: 20px;
-        }
-        
-        .btn-submit {
-            font-weight: 600;
             color: #fff;
             background-color: #007BFF;
             border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s ease-in-out;
         }
-        
-        .btn-submit:hover {
+
+        button:hover {
             background-color: #0056b3;
-        }
-        
-        .error-message {
-            color: red;
         }
     </style>
 </head>
 <body>
-    <?php
-    $msg = "";
-    $ademailmsg = "";
-    $adpasdmsg = "";
-    $emailmsg = "";
-    $pasdmsg = "";
-
-    if (!empty($_REQUEST['ademailmsg'])) {
-        $ademailmsg = $_REQUEST['ademailmsg'];
-    }
-
-    if (!empty($_REQUEST['adpasdmsg'])) {
-        $adpasdmsg = $_REQUEST['adpasdmsg'];
-    }
-
-    if (!empty($_REQUEST['emailmsg'])) {
-        $emailmsg = $_REQUEST['emailmsg'];
-    }
-
-    if (!empty($_REQUEST['pasdmsg'])) {
-        $pasdmsg = $_REQUEST['pasdmsg'];
-    }
-
-    if (!empty($_REQUEST['msg'])) {
-        $msg = $_REQUEST['msg'];
-    }
-    ?>
-
     <div class="container">
-        <div class="row">
-            <h4><?php echo $msg ?></h4>
-        </div>
-        <div class="row">
-            <div class="col-md-6 login-form">
-                <h3 class="login-heading">Admin Login</h3>
-                <form action="loginadmin_server_page.php" method="get">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
-                        <label class="error-message"><?php echo $ademailmsg ?></label>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
-                        <label class="error-message"><?php echo $adpasdmsg ?></label>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-submit" value="Login" />
-                    </div>
-                </form>
-            </div>
-            <div class="col-md-6 login-form">
-                <h3 class="login-heading">Student Login</h3>
-                <form action="login_server_page.php" method="get">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
-                        <label class="error-message"><?php echo $emailmsg ?></label>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
-                        <label class="error-message"><?php echo $pasdmsg ?></label>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-submit" value="Login" />
-                    </div>
-                </form>
-            </div>
-        </div>
+        <h1>Welcome to the Library Login Portal</h1>
+        <p>Please select your role to log in:</p>
+        <button onclick="window.location.href='admin_login.php'">Admin Login</button>
+        <button onclick="window.location.href='student_login.php'">Student Login</button>
     </div>
 </body>
 </html>
